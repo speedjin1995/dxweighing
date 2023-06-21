6ae44cbe2a5c3861cd7cd1584baac911832f24f6
speedjin1995 <speedjin1995@gmail.com>
2023-06-21 23:00:45 +0800

    fix some headers

--- menu.php
+++ menu.php
@@ -6,7 +6,8 @@
     </a>
 
     <ul class="middle-header">
-        <li><a href="s.php" class="nav-btn">Precision Scales</a>
+        <li>
+            <a href="s.php" class="nav-btn">Precision Scales</a>
             <a href="3.php" class="nav-btn">Platform Scales</a>
             <a href="x.php" class="nav-btn">Crane Weigher</a>
             <a href="y.php" class="nav-btn">Weigh-Bridges</a>
@@ -31,12 +32,10 @@
         <li class="middle-header-nav-link"><a href="#" class="nav-link">model y</a></li>
         <li class="middle-header-nav-link"><a href="#" class="nav-link">solar roof</a></li>
         <li><a href="index.php" class="nav-link">home</a></li>
-        <li><a href="table.php" class="nav-link">existing inventory</a></li>
-        <li><a href="s.php" class="nav-link">model s</a></li>
-        <li><a href="3.php" class="nav-link">model 3</a></li>
-        <li><a href="x.php" class="nav-link">model x</a></li>
-        <li><a href="y.php" class="nav-link">model y</a></li>
-        <li><a href="roof.php" class="nav-link">solar roof</a></li>
-        <li><a href="#" class="nav-link">charging</a></li>
+        <li><a href="s.php" class="nav-link">Precision Scales</a></li>
+        <li><a href="3.php" class="nav-link">Platform Scales</a></li>
+        <li><a href="x.php" class="nav-link">Crane Weigher</a></li>
+        <li><a href="y.php" class="nav-link">Weigh-Bridges</a></li>
+        <li><a href="roof.php" class="nav-link">Others</a></li>
     </ul>
 </nav>
--- roof.php
+++ roof.php
@@ -1,64 +1,13 @@
 <!DOCTYPE html>
 <html lang="en">
-
-<head>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width" />
-    <title>GSS-Tesla</title>
-    <link rel="icon" type="image/x-icon" href="assets/media/tesla_logo_icon_169601.png">
-    <link rel="stylesheet" href="assets/css/index.css" />
-    <link rel="stylesheet" href="assets/css/variable.css" />
-    <link rel="stylesheet" href="assets/css/header2.css" />
-    <link rel="stylesheet" href="assets/css/navigation.css" />
-    <link rel="stylesheet" href="assets/css/section.css" />
-    <link rel="stylesheet" href="assets/css/footer.css" />
-    <link rel="stylesheet" href="assets/css/responsive.css" />
-    <link rel="stylesheet" href="assets/css/s.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fullPage.js/3.1.2/fullpage.min.css"
-        integrity="sha512-4rPgyv5iG0PZw8E+oRdfN/Gq+yilzt9rQ8Yci2jJ15rAyBmF0HBE4wFjBkoB72cxBeg63uobaj1UcNt/scV93w=="
-        crossorigin="anonymous" referrerpolicy="no-referrer" /> 
-</head>
+<?php 
+    include 'header.php';
+?>
 
 <body>
-    <div class="blur-overlay"></div>
-
-    <header class="header">
-        <a href="index.php" class="left-header">
-            <img src="assets/media/tesla_logo.svg" alt="logo" class="logo">
-        </a>
-
-        <ul class="middle-header">
-            <li><a href="s.php" class="nav-btn">model s</a>
-                <a href="3.php" class="nav-btn">model 3</a>
-                <a href="x.php" class="nav-btn">model x</a>
-                <a href="y.php" class="nav-btn">model y</a>
-            </li>
-        </ul>
-
-        <div class="right-header">
-            <a href="#" class="nav-btn menu-btn">menu</a>
-        </div>
-    </header>
-
-    <nav class="navigation">
-        <div class="close-btn-container">
-            <img src="assets/media/close_icon.svg" alt="close button" class="close-btn">
-        </div>
-
-        <ul>
-            <li class="middle-header-nav-link"><a href="#" class="nav-link">model s</a></li>
-            <li class="middle-header-nav-link"><a href="#" class="nav-link">model 3</a></li>
-            <li class="middle-header-nav-link"><a href="#" class="nav-link">model x</a></li>
-            <li class="middle-header-nav-link"><a href="#" class="nav-link">model y</a></li>
-            <li><a href="index.php" class="nav-link">home</a></li>
-            <li><a href="table.php" class="nav-link">existing inventory</a></li>
-            <li><a href="s.php" class="nav-link">model s</a></li>
-            <li><a href="3.php" class="nav-link">model 3</a></li>
-            <li><a href="x.php" class="nav-link">model x</a></li>
-            <li><a href="y.php" class="nav-link">model y</a></li>
-            <li><a href="#" class="nav-link">charging</a></li>
-        </ul>
-    </nav>
+<?php 
+    include 'menu2.php';
+?>
 
     <section class="section three">
         <div class="content">
