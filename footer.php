0536e96c2f3f0c2c99241dea43e0c73930a5a580
Xin Yi <xinico1213@gmail.com>
2023-07-06 21:20:30 +0800

    fix contact page

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
 <div>
     <ul>
         <li><a href="#">Dx-Resources © 2023</a></li>
-        <li><a href="#">contact</a></li>
+        <li><a href="actionpage.php">contact</a></li>
         <li><a href="#">get updates</a></li>
         <li><a href="#">locations</a></li>
         <li><a href="#">about us</a></li>
